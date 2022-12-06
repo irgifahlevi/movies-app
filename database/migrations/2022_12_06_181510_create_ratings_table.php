76d906dd8dd5480ac26c6ec40a22a4f618b80056
irgifahlevi <irgifahlevi5@gmail.com>
2022-12-07 01:22:50 +0700

    frist commit

---
+++ database/migrations/2022_12_06_181510_create_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->string('rating')->nullable();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('movie_id')->references('id')->on('movies');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
